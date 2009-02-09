f8ec39e20acf08aee229b51d909fd36a5a9c9c72
Dave Evans <dave@swerveconcepts.com>
2009-02-09 14:15:00 +0000

    Improvements to Layout controller

--- controllers/layout.php
+++ controllers/layout.php
@@ -2,111 +2,106 @@
 abstract class Layout_Controller extends Controller
 {
 	public $template = 'templates/main';
-	
+
 	public $content;
-	
+
 	private $_path;
 	private $_url;
-	
+
+
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		$this->content = new View;
-		
+
 		if($this->template)
 		{
 			$this->template = new View($this->template);
 			$this->template->bind('content', $this->content);
 		}
-		
+
 		// Render the template immediately after the controller method
 		Event::add('system.post_controller', array($this, '_render'));
 	}
-	
-	
-	/*
-	*	If no matching method is defined, but an appropriately named view exists
-	*	then we load that view as $this->content and render the template
-	*/
-	public function __call($method, $args)
-	{
-		// find the view name that corresponds to this method call
-		$view = $this->_path().'/'.$method.( count($args) ? '/'.join('/', $args) : '');
-		
-		if(Kohana::find_file('views', $view, FALSE)) // if it exists ...
-		{
-			$this->content->set_filename($view); // ... display it
-		}
-		else
-		{
-			parent::__call($method, $args);
-		}
-	}
-	
-	
+
+
 	public function _render()
 	{
-		// if the content view hasn't been set yet ...		
+		// If $content is a view object but doesn't have a view file defined ...
 		if($this->content instanceof View AND ! $this->content->kohana_filename)
 		{
-			// ... use the default one
-			$this->content->set_filename($this->_path().'/'.Router::$method);
+			// ... use the default one based on controller and method name
+			$this->content->set_filename($this->_path(Router::$method));
 		}
-		
-		if($this->template instanceof View) // if there's a template defined ...
+
+		if($this->template instanceof View) // If there's a template defined ...
 		{
-			$this->template->render(TRUE); // ... render it 
+			$this->template->render(TRUE); // ... render it
 		}
 		elseif($this->content instanceof View)
 		{
 			$this->content->render(TRUE); // ... otherwise just render the content view
 		}
-		
+
 	}
-	
-	
-	public function _url($extra = '')
+
+
+	public function _url($segments = '', $protocol = NULL)
 	{
+		// Find the controller URL, if not already found and chached
 		if ( ! isset($this->_url))
 		{
-			// Get the original (pre-routed) url segments
-			$segments = Router::$segments;
-			
+			// Get the original (pre-routed) URL segments
+			$original = Router::$segments;
+
 			// Get the method name and arguments in a single list
 			$arguments = array_merge((array) Router::$method, Router::$arguments);
-			
-			// Remove the method name and arguments from the original url
+
+			// Remove the method name and arguments from the original URL
 			while($argument = array_pop($arguments))
-			{	
-				if ($argument == end($segments)) array_pop($segments);
+			{
+				if ($argument == end($original)) array_pop($original);
 			}
-			
-			// What's left should be the controller url
-			$this->_url = join('/', $segments);
+
+			// What's left should be the controller URL
+			$this->_url = join('/', $original);
 		}
-		
-		if ($extra != '')
+
+		if ($segments != '')
 		{
-			$extra = '/'.trim($extra, '/');
+			$segments = '/'.trim($segments, '/');
 		}
-		
-		return $this->_url.$extra;
+
+		$url = $this->_url.$segments;
+
+		return $protocol === NULL ? $url : url::site($url, $protocol);
 	}
-	
-	
-	public function _path()
+
+
+	public function _path($path = '')
 	{
-		// Router::$controller_path is the full pathname of the controller
-		// we just want the bits after the /controllers/ segment
-		$path = substr(Router::$controller_path, strrpos(Router::$controller_path, '/controllers/') + 13);
-		
-		// chop off the last bit - the controller filename - to leave the controller directory
-		$path = array_slice(explode('/', $path), 0, -1);
-		
-		// add on the controller name
-		$path[] = Router::$controller;
-		
-		return join('/', $path);
+		// Find the controller path, if not already found and cached
+		if ( ! isset($this->_path))
+		{
+			// Router::$controller_path is the full pathname of the controller
+			// we just want the bits after the /controllers/ segment
+			$this->_path = substr(Router::$controller_path, strrpos(Router::$controller_path, '/controllers/') + 13);
+
+			// Chop off the last bit - the controller filename - to leave the controller directory
+			$this->_path = array_slice(explode('/', $this->_path), 0, -1);
+
+			// Add on the controller name
+			$this->_path[] = Router::$controller;
+
+			$this->_path = join('/', $this->_path);
+		}
+
+		if ($path != '')
+		{
+			$path = '/'.trim($path, '/');
+		}
+
+		return $this->_path.$path;
 	}
 }
