28dce79b234a1713b5d7d10ed701ac7152a6b250
Dave Evans <dave@swerveconcepts.com>
2009-06-15 13:08:22 +0100

    Added support for multiple groups of notices; removed support for setting notices view at render time which was useless (and hence unused)

--- helpers/notices.php
+++ helpers/notices.php
@@ -1,62 +1,78 @@
 <?php
 class notices_Core
 {
-	public static function confirm($message)
+	public static function confirm($message, $group = 'default')
 	{
-		notices::set('heading', $message);
-		notices::set('class', 'confirmation');
+		notices::set('heading', $message, $group);
+		notices::set('class', 'confirmation', $group);
 	}
 	
 	
-	public static function error($message)
+	public static function error($message, $group = 'default')
 	{
-		notices::set('heading', $message);
-		notices::set('class', 'error');
+		notices::set('heading', $message, $group);
+		notices::set('class', 'error', $group);
 	}
 	
 	
-	public static function errors($list)
+	public static function errors($list, $group = 'default')
 	{
-		notices::set('list', (array) $list);
-		notices::set('class', 'error');
+		notices::set('list', (array) $list, $group);
+		notices::set('class', 'error', $group);
 	}
 		
 	
-	public static function set($key, $value)
+	public static function set($key, $value, $group = 'default')
 	{
 		// Get the notices array out of the session, blank array as default
 		$notices = Session::instance()->get('notices', array());
 		
+		if( ! isset($notices[$group]))
+		{
+			$notices[$group] = array();
+		}
+		
 		// If $value is an array we merge it with the existing values
 		if(is_array($value)) 
 		{
-			$notices[$key] = isset($notices[$key]) ? (array) $notices[$key] : array();
+			// Make sure the appropriate key is set and is an array
+			$notices[$group][$key] = isset($notices[$group][$key]) ? (array) $notices[$group][$key] : array();
 			$value = array_values($value);
-			$notices[$key] = array_merge($notices[$key], $value);
+			$notices[$group][$key] = array_merge($notices[$group][$key], $value);
 		}
 		else
 		{
-			$notices[$key] = $value;
+			$notices[$group][$key] = $value;
 		}
 		
 		Session::instance()->set('notices', $notices);
 	}
 	
 	
-	public static function render($view = FALSE)
+	public static function render($group = 'default')
 	{
-		// Check for existence of session to avoid creating one if not required
-		if (empty($_SESSION['session_id']) AND empty($_COOKIE[$name = Kohana::config('session.name')]) AND empty($_GET[$name]))
-		{
-			return;
-		}
+		if ( ! notices::session_exists()) return;
 		
 		$notices = Session::instance()->get_once('notices');
 		
-		if (empty($notices)) return;
-		
-		$view = empty($notices['view']) ? ($view ? $view : 'notices') : $notices['view'];
-		
-		return View::factory($view, $notices)->render();
+		if ( ! empty($notices) AND ! empty($notices[$group]))
+		{
+			$view = empty($notices[$group]['view']) ? 'notices' : $notices[$group]['view'];
+			return View::factory($view, $notices[$group])->render();
+		}
+	}
+	
+	// Check whether session exists without creating one
+	public static function session_exists()
+	{
+		if ( ! empty($_SESSION['session_id']))
+		{
+			return TRUE;
+		}
+		else
+		{
+			$session_name = Kohana::config('session.name');
+			return ( ! empty($_COOKIE[$session_name]) OR ! empty($_GET[$session_name]));
+		}
 	}
 }
