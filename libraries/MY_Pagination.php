0e07b9cb22d65ebe171f6a4a33489648ae1449f9
David Evans <dave@orman.(none)>
2010-03-11 13:02:44 +0000

    Improve Pagination docs and slightly refactor for clarity

--- libraries/MY_Pagination.php
+++ libraries/MY_Pagination.php
@@ -1,17 +1,58 @@
 <?php
 class Pagination extends Pagination_Core
 {
-	// Optional hash fragment to be added to end of URL e.g, #results
+	/**
+	 * @var  string  Optional hash fragment to be added to end of URL e.g, #results
+	 */
 	protected $fragment;
-
-	// Any extra settings to be passed to the pagination view
+	
+	/**
+	 * @var  array   Any extra settings to be passed to the pagination view
+	 */
 	protected $extras = array();
 
-
-	public function paginate(ORM $model)
+	/**
+	 * Accepts an ORM instance with query parameters set and returns the
+	 * current page of results e.g.,
+	 * 
+	 *    $query = ORM::factory('customer')->where('status', 'approved);
+	 *    $pages = new Pagination();
+	 *    $results = $pages->paginate($query);
+	 * 
+	 * @param    object  ORM
+	 * @return   object  ORM_Iterator
+	 */
+	public function paginate(ORM $query)
 	{
-		// Re-extract current page
-		// This will be have been set to 1 if the original total_items count was 0
+		// Work out what page we're currently on and set the SQL offsets
+		// appropriately.
+		//
+		// This is necessary because if you create a Pagination object
+		// without specifying the total pages, then current page is
+		// always set to 1.
+		$this->determine_current_page()
+
+		// Find the current page of results
+		$results = $query->find_all($this->items_per_page, $this->sql_offset);
+
+		// Set the correct total_item count
+		$this->total_items = $query->count_last_query();
+
+		// Re-initialize to ensure all values are set correctly
+		$this->initialize();
+
+		return $results;
+	}
+	
+	/**
+	 * Determines the current page, and sets the SQL offsets, in exactly
+	 * the same way as the original pagination library, except that this
+	 * method can be run **before** we know the total number of pages.
+	 * 
+	 * @return   void
+	 */
+	public function determine_current_page()
+	{
 		if ($this->query_string !== '')
 		{
 			$this->current_page = isset($_GET[$this->query_string]) ? (int) $_GET[$this->query_string] : 1;
@@ -25,24 +66,19 @@ class Pagination extends Pagination_Core
 
 		// Reset the sql offset based on current page
 		$this->sql_offset = (int) ($this->current_page - 1) * $this->items_per_page;
-
-		// Find the current page of results
-		$results = $model->find_all($this->items_per_page, $this->sql_offset);
-
-		// Set the correct total_item count
-		$this->total_items = $model->count_last_query();
-
-		// Re-initialize to ensure all values are set correctly
-		$this->initialize();
-
-		return $results;
 	}
 
-
+	/**
+	 * Extends the original `initialize()` method to append the hash
+	 * fragment to the URL.
+	 * 
+	 * @return   void
+	 */
 	public function initialize($config = array())
 	{
 		parent::initialize($config);
-
+		
+		// Add the fragment to the URL, if set
 		if( ! empty($this->fragment))
 		{
 			$this->url .= '#'.ltrim($this->fragment, '#');
